7438c9aaa50688defc11f77b4ba657f16d30d622
Herman Makhiyanov <hermanvfx@outlook.com>
2023-06-09 22:53:02 +0300

    inicial commit

---
+++ src/main/java/com/example/microserviceexample/MicroServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceExampleApplication.class, args);
+    }
+
+}
